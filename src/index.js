eba2a963382726f6476e5cc88ff8cac90022c0e4
Kenebebh <kenebebhbanigo@gmail.com>
2022-06-09 15:01:35 +0100

    my solution

---
+++ src/index.js
@@ -0,0 +1,25 @@
+// complete the function
+function prime(num) {
+  // code goes here
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return num > 1;
+}
+
+function solution(arg) {
+  return prime(arg);
+}
+
+if (typeof require !== "undefined" && require.main === module) {
+  if (process.argv.length <= 2) {
+    throw Error("Require argument");
+  }
+
+  const arg = process.argv[2];
+  console.log(solution(arg));
+}
+
+module.exports = { solution };
